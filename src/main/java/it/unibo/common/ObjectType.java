c2fa3beb0c52911fba4f7558a6495ad70d71b06f
Gaburi14 <94144396+Gaburi14@users.noreply.github.com>
2024-01-30 10:47:46 +0100

    Add files via upload

---
+++ src/main/java/it/unibo/common/ObjectType.java
@@ -0,0 +1,13 @@
+package it.unibo.common
+
+/**
+ * Model the concept of object type present in the game
+ * 
+ */
+    enum TypeObject{
+        CHARACTER,
+        TRAP,
+        ENEMIES,
+        MAPELENET
+
+    }
