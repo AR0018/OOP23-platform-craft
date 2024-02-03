e2983b57f6756583aa14e20a385573312670e380
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2024-02-03 18:50:45 +0100

    Fixed enum Direction

--- src/main/java/it/unibo/model/physics/api/Direction.java
+++ src/main/java/it/unibo/model/physics/api/Direction.java
@@ -3,12 +3,21 @@ package it.unibo.model.physics.api;
 /**
  * Models the concept of direction in 4 dimensions.
  */
-public interface Direction {
-
-    enum Dir{
-        UP,
-        DOWN,
-        RIGHT,
-        LEFT;
-    }
+public enum Direction {
+    /**
+     * The direction UP.
+     */
+    UP,
+    /**
+     * The direction DOWN.
+     */
+    DOWN,
+    /**
+     * The direction LEFT.
+     */
+    LEFT,
+    /**
+     * The direction RIGHT.
+     */
+    RIGHT;
 }
