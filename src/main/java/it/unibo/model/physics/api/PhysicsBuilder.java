fb1cef771d67ba5b1f509b67ca3782a1823003d5
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2024-02-03 12:15:25 +0100

    Added interface PhysicsBuilder

---
+++ src/main/java/it/unibo/model/physics/api/PhysicsBuilder.java
@@ -0,0 +1,58 @@
+package it.unibo.model.physics.api;
+
+import it.unibo.model.entities.api.GameObject;
+
+/**
+ * This builder is used to configure an instance of Physics with the desired behaviour.
+ * The default Physics has a linear motion on both X and Y axis.
+ */
+public interface PhysicsBuilder {
+
+    /**
+     * Sets the GameObject that the Physics is associated to.
+     * This method must be called once in the creation of the Physics, 
+     * otherwise the configuration is not complete.
+     * @return this builder, for method chaining
+     * @param obj the GameObject
+     */
+    PhysicsBuilder setGameObject(GameObject obj);
+
+    /**
+     * Sets the speed of movement on the X axis to the desired configuration.
+     * The default configuration is MEDIUM
+     * @param speed
+     * @return this builder, for method chaining
+     */
+    PhysicsBuilder setSpeedOnX(Speed speed); //TODO: review design for Speed
+
+    /**
+     * Sets the speed of movement on the Y axis to the desired configuration.
+     * The default configuration is MEDIUM
+     * @param speed
+     * @return this builder, for method chaining
+     */
+    PhysicsBuilder setSpeedOnY(Speed speed); //TODO: review design for Speed
+
+    /**
+     * Sets the Physics so that the movement on the X axis is accelerated.
+     * @return this builder, for method chaining
+     * @throws IllegalStateException if this method is called more than once
+     */
+    PhysicsBuilder addAccelerationOnX();
+
+    /**
+     * Sets the Physics so that the GameObject falls with a constant acceleration.
+     * @return this builder, for method chaining
+     * @throws IllegalStateException if this method is called more than once
+     */
+    PhysicsBuilder addFallingPhysics();
+
+
+    /**
+     * Creates the Physics with the desired configuration.
+     * @return the instance of Physics
+     * @throws IllegalStateException if the configuration is invalid (the method setGameObject() has never been called)
+     */
+    Physics create();
+
+}
---
+++ src/main/java/it/unibo/model/physics/api/Speed.java
@@ -0,0 +1,8 @@
+package it.unibo.model.physics.api;
+
+/**
+ * Defines the possible configurations for the movement speed of an object
+ */
+public enum Speed { //TODO: review design for Speed
+    FAST, MEDIUM, SLOW;
+}
