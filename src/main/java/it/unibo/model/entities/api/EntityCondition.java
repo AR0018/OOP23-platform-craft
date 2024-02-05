70ba5cab0c351d115abbb5a0ae20f3740c9cf150
Simone Carpi <simone.carpi@studio.unibo.it>
2024-02-05 17:49:50 +0100

    Added new enum EntityCondition to understand if it's alive or not

---
+++ src/main/java/it/unibo/model/entities/api/EntityCondition.java
@@ -0,0 +1,15 @@
+package it.unibo.model.entities.api;
+
+/**
+* Used to list the different states of the entity.
+*/
+public enum EntityCondition {
+    /**
+     * It's alive.
+     */
+    ALIVE,
+    /**
+     * It's dead.
+     */
+    DEAD;
+}
