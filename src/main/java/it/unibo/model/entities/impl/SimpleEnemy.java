6a6ba7eec1226c69e7b8991e28b97115340f51ef
Simone Carpi <simone.carpi@studio.unibo.it>
2024-02-05 18:41:38 +0100

    Wrote methods for SimpleEnemy and added some information about movement

--- src/main/java/it/unibo/model/entities/impl/SimpleEnemy.java
+++ src/main/java/it/unibo/model/entities/impl/SimpleEnemy.java
@@ -3,6 +3,12 @@ package it.unibo.model.entities.impl;
 import it.unibo.common.EntityType;
 import it.unibo.model.collisions.api.CollisionBox;
 import it.unibo.model.entities.api.Enemy;
+import it.unibo.model.entities.api.Character;
+import it.unibo.model.entities.api.EntityCondition;
+import it.unibo.model.entities.api.GameEntity;
+import it.unibo.model.entities.api.MapElement;
+import it.unibo.model.level.api.Level;
+import it.unibo.model.physics.api.Direction;
 import it.unibo.model.physics.api.Physics;
 import it.unibo.model.physics.api.PhysicsBuilder;
 import it.unibo.model.physics.api.Position;
@@ -10,13 +16,20 @@ import it.unibo.model.physics.api.Position;
 public class SimpleEnemy implements Enemy{
 
     private final Physics physics;
+    private final Character player;
+    private Level level;
     private CollisionBox box;
     private Position position;
     private PhysicsBuilder physicsBuilder;
-    //private EntityCondition condition;
+    private EntityCondition condition;
+    private boolean CollisionUP;
+    private Direction direction;
     
-    public SimpleEnemy(final Position position){        //TODO: set an enum Condition both for Char and Enemy
+    public SimpleEnemy(final Position position, final Character player){
+        this.player = player;
         this.position = position;
+        this.direction = Direction.RIGHT;
+        this.condition = EntityCondition.ALIVE;
         this.physics = this.physicsBuilder.setGameObject(this).create();
     }
 
@@ -32,14 +45,14 @@ public class SimpleEnemy implements Enemy{
 
     @Override
     public boolean isAlive() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAlive'");
+        return this.condition.equals(EntityCondition.ALIVE);
     }
 
     @Override
     public void updateState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateState'");
+        moveEnemy();
+        this.physics.calculateMovement();
+        checkEnemyCollisions();
     }
 
     @Override
@@ -51,5 +64,28 @@ public class SimpleEnemy implements Enemy{
     public CollisionBox getCollisionBox() {
         return this.box;
     }
-    
+
+    private void moveEnemy() {
+        this.physics.setMovement(this.direction);
+    }
+
+    private void checkEnemyCollisions(){
+        this.box.checkCollisions(this.level.getGameEntities());     //TODO: understand what checkCollisions does
+        if(!this.box.getCollisions().isEmpty()) {
+            if (this.box.isCollidingWith(this.player)) {
+                checkEnemyIsDead();
+            } else {
+                
+            }
+        }
+    }
+
+    private void checkEnemyIsDead() {
+        this.CollisionUP = this.box.getCollisions().stream()
+                    .filter(x -> x instanceof Character)
+                    .anyMatch(x -> x.getDirection().equals(Direction.UP));
+        if (this.CollisionUP) {
+            this.condition = EntityCondition.DEAD;
+        }
+    }
 }
