554a7609447bbc5fbbb00725b17c1d641584b636
Simone Carpi <simone.carpi@studio.unibo.it>
2024-02-08 17:28:07 +0100

    Added ViewImpl

---
+++ src/main/java/it/unibo/view/impl/ViewImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.view.impl;
+
+import java.util.Set;
+import it.unibo.common.SimpleEntity;
+import it.unibo.view.api.View;
+
+public class ViewImpl implements View{
+
+    @Override
+    public void displayStart() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayStart'");
+    }
+
+    @Override
+    public void displayWin() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayWin'");
+    }
+
+    @Override
+    public void displayGameOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayGameOver'");
+    }
+
+    @Override
+    public void render(Set<SimpleEntity> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+    
+}
