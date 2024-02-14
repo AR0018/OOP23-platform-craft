f4086dcf295d8233098b05b636a156263fdd7222
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2024-02-14 12:31:46 +0100

    Added SerializerImpl

---
+++ src/main/java/it/unibo/controller/impl/SerializerImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.controller.impl;
+
+import java.io.File;
+import java.util.Set;
+
+import it.unibo.common.SimpleEntity;
+import it.unibo.controller.api.LevelSerializer;
+
+public class SerializerImpl implements LevelSerializer {
+
+    @Override
+    public void saveLevel(Set<SimpleEntity> entities, File file) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveLevel'");
+    }
+
+    @Override
+    public Set<SimpleEntity> loadLevel(File file) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadLevel'");
+    }
+
+}
