b5377a9b0430d3c9b9968da0ce883713e8fd30fe
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2024-02-14 13:55:48 +0100

    Implemented SerializerImpl

--- src/main/java/it/unibo/controller/impl/SerializerImpl.java
+++ src/main/java/it/unibo/controller/impl/SerializerImpl.java
@@ -4,24 +4,29 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import it.unibo.common.SimpleEntity;
 import it.unibo.controller.api.LevelSerializer;
 
-public class SerializerImpl implements LevelSerializer {
+/**
+ * Implementation of LevelSerializer which saves json files using Jackson library.
+ */
+public final class SerializerImpl implements LevelSerializer {
 
     @Override
-    public void saveLevel(Set<SimpleEntity> entities, File file) {
-        if(!file.exists()) {
-            //file.createNewFile(); TODO: does file need to be created?
-        }
-        //new ObjectMapper().writeValue(file, entities);
+    public void saveLevel(final Set<SimpleEntity> entities, final File file) throws IOException {
+        /*
+        if (!file.exists()) {
+            file.createNewFile(); TODO: does file need to be created?
+        }*/
+        new ObjectMapper().writeValue(file, entities);
     }
 
     @Override
-    public Set<SimpleEntity> loadLevel(File file) {
-        throw new UnsupportedOperationException("Unimplemented method 'loadLevel'");
+    public Set<SimpleEntity> loadLevel(final File file) throws IOException {
+        return new ObjectMapper().readValue(file, new TypeReference<Set<SimpleEntity>>() { } );
     }
 
 }
