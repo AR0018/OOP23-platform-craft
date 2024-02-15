819d4b66d71d151ebf83e2c720e8056a20e60334
Alessandro Ronchi <alessandro.ronchi7@studio.unibo.it>
2024-02-15 11:47:10 +0100

    Implemented notifyCommand in LevelRunnerImpl

--- src/main/java/it/unibo/controller/impl/LevelRunnerImpl.java
+++ src/main/java/it/unibo/controller/impl/LevelRunnerImpl.java
@@ -1,25 +1,23 @@
 package it.unibo.controller.impl;
 
 import java.io.File;
+import java.util.Objects;
 
 import it.unibo.controller.api.Command;
 import it.unibo.controller.api.LevelRunner;
+import it.unibo.view.api.View;
 
 /**
  * Implementation of a LevelRunner.
  */
 public final class LevelRunnerImpl implements LevelRunner {
-
-    /*
-    private Engine engine;
+    //private Engine engine;
     private RunnerAgent runner;
-    */
 
     /**
      * The constructor of this LevelRunner.
      */
-    public LevelRunnerImpl() {
-
+    public LevelRunnerImpl(final View view) {
     }
 
     @Override
@@ -30,8 +28,7 @@ public final class LevelRunnerImpl implements LevelRunner {
 
     @Override
     public void notifyCommand(final Command command) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'notifyCommand'");
+        this.runner.notifyCommand(Objects.requireNonNull(command));
     }
 
     @Override
